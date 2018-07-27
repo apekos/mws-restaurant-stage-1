2ef51ba8a6a2b5f5b2306b33e50a9d570b77c87e
Alexandros Pekos <apekos7@gmail.com>
2018-07-28 02:43:16 +0300

    Added Mapbox API and viewport meta tag. Home page responsive.

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYXBla29zIiwiYSI6ImNqazJvZTcydzB1Yjgzcm9nNHlsanF5a3QifQ.RwYK5ulFGyPyPQDYQGWF0Q',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
