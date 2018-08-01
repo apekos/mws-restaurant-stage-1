8322bc29f9f9f816f583558afb39bb8024029193
Alexandros Pekos <apekos7@gmail.com>
2018-08-01 04:39:40 +0300

    Add service worker

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+/**
+ * Registering the service worker.
+ */
+navigator.serviceWorker.register('/sw.js').then(function(reg) {
+  console.log('Registration worked!');
+}).catch(function() {
+  console.log('Registration failed!');
+})
+
 let restaurants,
   neighborhoods,
   cuisines
